1660f1c1893f4aee7cb0dbead807df4d58e3fae6
faketuna <faketuna.st@gmail.com>
2024-05-12 21:41:50 +0900

    feat: initial commit

---
+++ LupercaliaMGCore/LupercaliaMGCore.cs
@@ -0,0 +1,22 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace LupercaliaMGCore {
+    public class LupercaliaMGCore: BasePlugin
+    {
+        public override string ModuleName => "Lupercalia MG Core";
+
+        public override string ModuleVersion => "0.0.1";
+
+        public override string ModuleAuthor => "faketuna";
+
+        public override string ModuleDescription => "Provides core MG feature in CS2 with CounterStrikeSharp";
+
+        // Feature classes
+
+
+
+        public override void Load(bool hotReload)
+        {
+        }
+    }
+}
