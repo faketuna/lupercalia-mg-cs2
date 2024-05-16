36d1804864913701b58e994f3dd108003d0e0220
faketuna <faketuna.st@gmail.com>
2024-05-16 22:27:47 +0900

    chore: add template class for copy pasting

---
+++ LupercaliaMGCore/ClassTemplate.cs
@@ -0,0 +1,12 @@
+namespace LupercaliaMGCore {
+    public class ClassTemplate {
+        private LupercaliaMGCore m_CSSPlugin;
+
+        public ClassTemplate(LupercaliaMGCore plugin) {
+            m_CSSPlugin = plugin;
+        }
+
+        public void initialize() {
+        }
+    }
+}
