7acc5c5b9f37c2f1259d29934573233fe18b213c
faketuna <faketuna.st@gmail.com>
2024-05-19 19:25:57 +0900

    feat: able to find looking entity. but spec_player is not work as expected like CS:GO

--- LupercaliaMGCore/EasySpectate.cs
+++ LupercaliaMGCore/EasySpectate.cs
@@ -1,14 +1,34 @@
+using System.Runtime.InteropServices;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
 
 namespace LupercaliaMGCore {
     public class EasySpectate {
         private LupercaliaMGCore m_CSSPlugin;
         private Dictionary<CCSPlayerController ,double> playerLastButtonPressedTime = new Dictionary<CCSPlayerController, double>();
         private readonly double buttonCooldown = 1.0D;
+        private int OFFSET_CGameRules_FindPickerEntity;
 
-        public EasySpectate(LupercaliaMGCore plugin) {
+        public EasySpectate(LupercaliaMGCore plugin, bool hotReload) {
             m_CSSPlugin = plugin;
+
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                OFFSET_CGameRules_FindPickerEntity = 27;
+            } else {
+                OFFSET_CGameRules_FindPickerEntity = 28;
+            }
+
+            if(hotReload) {
+                List<CCSPlayerController> players = Utilities.GetPlayers();
+                foreach (CCSPlayerController player in players) {
+                    if(player == null || !player.IsValid)
+                        continue;
+
+                    Server.PrintToChatAll("Hooking!");
+                    hookPlayerButton(player);
+                }
+            }
         }
 
         private void hookPlayerButton(CCSPlayerController player) {
@@ -25,7 +45,36 @@ namespace LupercaliaMGCore {
                     return;
                 
                 // TODO. Will be implemented after Trace Ray feature implemented in CounterStrikeSharp.
+
+                CCSPlayerController? targetPlayer = GetClientAimTarget(player);
+
+                if(targetPlayer == null)
+                    return;
+                
+                player.ExecuteClientCommandFromServer($"spec_player {targetPlayer.PlayerName}");
+                player.PrintToChat($"{targetPlayer.PlayerName} is in your LOS!");
             });
+        }
+
+        // Code from CounterStrikeSharp official Discord
+        // Thanks to xstage.
+        // https://discord.com/channels/1160907911501991946/1175947333880524962/1230542480903110716
+        public CCSPlayerController? GetClientAimTarget(CCSPlayerController player)
+        {
+            var GameRules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").FirstOrDefault()?.GameRules;
+
+            if (GameRules is null)
+                return null;
+
+            VirtualFunctionWithReturn<IntPtr, IntPtr, IntPtr> findPickerEntity = new(GameRules.Handle, OFFSET_CGameRules_FindPickerEntity);
+            var target = new CBaseEntity(findPickerEntity.Invoke(GameRules.Handle, player.Handle));
+
+            if (target.DesignerName is "player")
+            {
+                return target.As<CCSPlayerPawn>().OriginalController.Value;
+            }
+
+            return null;
         }
     }
 }
