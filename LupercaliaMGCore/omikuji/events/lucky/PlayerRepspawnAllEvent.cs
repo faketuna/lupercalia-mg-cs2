2f4729df9dff5575f4c96fc1f405223a5c908340
faketuna <faketuna.st@gmail.com>
2024-05-28 20:05:11 +0900

    feat: add all player respawn event in omikuji

--- LupercaliaMGCore/omikuji/events/lucky/PlayerRepspawnAllEvent.cs
+++ LupercaliaMGCore/omikuji/events/lucky/PlayerRepspawnAllEvent.cs
@@ -1,4 +1,42 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Microsoft.Extensions.Logging;
+
 namespace LupercaliaMGCore {
     public static partial class OmikujiEvents {
+
+        [OmikujiFunc("All Player Respawn Event", OmikujiType.EVENT_LUCKY)]
+        public static void PlayerRespawnAllEvent(CCSPlayerController client) {
+            LupercaliaMGCore.getInstance().Logger.LogDebug("Player drew a omikuji and invoked All player respawn event.");
+
+            CCSPlayerController? alivePlayer = null;
+
+            foreach(CCSPlayerController cl in Utilities.GetPlayers()) {
+                if(!cl.IsValid || cl.IsBot || cl.IsHLTV)
+                    continue;
+
+                if(cl.PawnIsAlive) {
+                    alivePlayer = cl;
+                    break;
+                }
+            }
+
+            if(alivePlayer == null) {
+                LupercaliaMGCore.getInstance().Logger.LogDebug("All player respawn event failed due to no one player is alive.");
+                return;
+            }
+
+            foreach(CCSPlayerController cl in Utilities.GetPlayers()) {
+                if(!cl.IsValid || cl.IsBot || cl.IsHLTV)
+                    continue;
+
+                if(cl.PawnIsAlive)
+                    continue;
+
+                cl.Respawn();
+                cl.Teleport(alivePlayer.PlayerPawn.Value!.AbsOrigin, alivePlayer.PlayerPawn.Value.AbsRotation);
+                cl.PrintToChat($"{Omikuji.CHAT_PREFIX} {client.PlayerName} have drew the mega luck! re-spawning the all players!!!");
+            }
+        }
     }
 }
