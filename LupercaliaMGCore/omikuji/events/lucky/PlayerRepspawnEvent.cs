629b3d1c38554e6b7f3cdbf41dc4b5d905e9777d
faketuna <faketuna.st@gmail.com>
2024-05-28 20:04:56 +0900

    feat: add player respawn event in omikuji

--- LupercaliaMGCore/omikuji/events/lucky/PlayerRepspawnEvent.cs
+++ LupercaliaMGCore/omikuji/events/lucky/PlayerRepspawnEvent.cs
@@ -1,4 +1,33 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using Microsoft.Extensions.Logging;
+
 namespace LupercaliaMGCore {
     public static partial class OmikujiEvents {
+
+        [OmikujiFunc("Player Respawn Event", OmikujiType.EVENT_LUCKY)]
+        public static void playerRespawnEvent(CCSPlayerController client) {
+            LupercaliaMGCore.getInstance().Logger.LogDebug("Player drew a omikuji and invoked Player respawn event");
+
+            string msg = "";
+
+            if(!client.PawnIsAlive) {
+                msg = $"{Omikuji.CHAT_PREFIX} Lucky! {client.PlayerName} have re-spawned!";
+            } else {
+                msg = $"{Omikuji.CHAT_PREFIX} Lucky! {client.PlayerName} have draw a fortune! But how unfortunate nothing happened because {client.PlayerName} are still alive.";
+            }
+
+            foreach(CCSPlayerController cl in Utilities.GetPlayers()) {
+                if(!cl.IsValid || cl.IsBot || cl.IsHLTV)
+                    continue;
+                
+                if(!client.PawnIsAlive && cl.PawnIsAlive) {
+                    client.Respawn();
+                    client.Teleport(cl.PlayerPawn!.Value!.AbsOrigin);
+                }
+
+                cl.PrintToChat($"{Omikuji.CHAT_PREFIX} Lucky! {client.PlayerName} have re-spawned!");
+            }
+        }
     }
 }
